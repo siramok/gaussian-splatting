a5e5e2b0c2bc4da84e39ea213cbc8195b0ac0637
Andres Sewell <sewellandres@gmail.com>
2024-06-12 11:21:39 -0500

    Improved camera

--- scene/cameras.py
+++ scene/cameras.py
@@ -3,7 +3,7 @@
 # GRAPHDECO research group, https://team.inria.fr/graphdeco
 # All rights reserved.
 #
-# This software is free for non-commercial, research and evaluation use 
+# This software is free for non-commercial, research and evaluation use
 # under the terms of the LICENSE.md file.
 #
 # For inquiries contact  george.drettakis@inria.fr
@@ -14,11 +14,25 @@ from torch import nn
 import numpy as np
 from utils.graphics_utils import getWorld2View2, getProjectionMatrix
 
+
 class Camera(nn.Module):
-    def __init__(self, colmap_id, R, T, FoVx, FoVy, cx, cy, image, gt_alpha_mask,
-                 image_name, uid,
-                 trans=np.array([0.0, 0.0, 0.0]), scale=1.0, data_device = "cuda"
-                 ):
+    def __init__(
+        self,
+        colmap_id,
+        R,
+        T,
+        FoVx,
+        FoVy,
+        cx,
+        cy,
+        image,
+        gt_alpha_mask,
+        image_name,
+        uid,
+        trans=np.array([0.0, 0.0, 0.0]),
+        scale=1.0,
+        data_device="cuda",
+    ):
         super(Camera, self).__init__()
 
         self.uid = uid
@@ -35,7 +49,9 @@ class Camera(nn.Module):
             self.data_device = torch.device(data_device)
         except Exception as e:
             print(e)
-            print(f"[Warning] Custom device {data_device} failed, fallback to default cuda device" )
+            print(
+                f"[Warning] Custom device {data_device} failed, fallback to default cuda device"
+            )
             self.data_device = torch.device("cuda")
 
         self.original_image = image.clamp(0.0, 1.0).to(self.data_device)
@@ -45,25 +61,57 @@ class Camera(nn.Module):
         if gt_alpha_mask is not None:
             self.original_image *= gt_alpha_mask.to(self.data_device)
         else:
-            self.original_image *= torch.ones((1, self.image_height, self.image_width), device=self.data_device)
+            self.original_image *= torch.ones(
+                (1, self.image_height, self.image_width), device=self.data_device
+            )
 
-        self.zfar = 100.0
+        self.zfar = 1000.01
         self.znear = 0.01
 
         self.trans = trans
         self.scale = scale
 
-        self.world_view_transform = torch.tensor(getWorld2View2(R, T, trans, scale)).transpose(0, 1).cuda()
-        self.projection_matrix = getProjectionMatrix(znear=self.znear, zfar=self.zfar,
-                                                     fovX=self.FoVx, fovY=self.FoVy,
-                                                     cx=self.cx, cy=self.cy).transpose(0, 1).cuda()
-        self.full_proj_transform = (self.world_view_transform.unsqueeze(0).bmm(self.projection_matrix.unsqueeze(0))).squeeze(0)
-        self.camera_center = self.world_view_transform.inverse()[3, :3]
+        self.world_view_transform = (
+            torch.tensor(getWorld2View2(R, T, trans, scale)).transpose(0, 1).cuda()
+        )
+        self.projection_matrix = (
+            getProjectionMatrix(
+                fovy=self.FoVy,
+                aspect=(800 / 600),
+                zNear=self.znear,
+                zFar=self.zfar,
+            )
+            .transpose(0, 1)
+            .cuda()
+        )
+        self.full_proj_transform = (
+            self.world_view_transform.unsqueeze(0).bmm(
+                self.projection_matrix.unsqueeze(0)
+            )
+        ).squeeze(0)
+        self.camera_center = self.world_view_transform[3, :3]
+
+    def from_vtk(self, wvt, proj, pos):
+        self.world_view_transform = torch.tensor(wvt).transpose(0, 1).cuda()
+        self.projection_matrix = torch.tensor(proj).transpose(0, 1).cuda()
+        self.full_proj_transform = torch.tensor(proj).transpose(0, 1).cuda()
+        self.camera_center = torch.tensor(pos).cuda()
+
 
 class MiniCam:
-    def __init__(self, width, height, fovy, fovx, znear, zfar, world_view_transform, full_proj_transform):
+    def __init__(
+        self,
+        width,
+        height,
+        fovy,
+        fovx,
+        znear,
+        zfar,
+        world_view_transform,
+        full_proj_transform,
+    ):
         self.image_width = width
-        self.image_height = height    
+        self.image_height = height
         self.FoVy = fovy
         self.FoVx = fovx
         self.znear = znear
@@ -72,4 +120,3 @@ class MiniCam:
         self.full_proj_transform = full_proj_transform
         view_inv = torch.inverse(self.world_view_transform)
         self.camera_center = view_inv[3][:3]
-
--- utils/graphics_utils.py
+++ utils/graphics_utils.py
@@ -3,7 +3,7 @@
 # GRAPHDECO research group, https://team.inria.fr/graphdeco
 # All rights reserved.
 #
-# This software is free for non-commercial, research and evaluation use 
+# This software is free for non-commercial, research and evaluation use
 # under the terms of the LICENSE.md file.
 #
 # For inquiries contact  george.drettakis@inria.fr
@@ -14,10 +14,12 @@ import math
 import numpy as np
 from typing import NamedTuple
 
+
 class BasicPointCloud(NamedTuple):
-    points : np.array
-    colors : np.array
-    normals : np.array
+    points: np.array
+    colors: np.array
+    normals: np.array
+
 
 def geom_transform_points(points, transf_matrix):
     P, _ = points.shape
@@ -28,6 +30,7 @@ def geom_transform_points(points, transf_matrix):
     denom = points_out[..., 3:] + 0.0000001
     return (points_out[..., :3] / denom).squeeze(dim=0)
 
+
 def getWorld2View(R, t):
     Rt = np.zeros((4, 4))
     Rt[:3, :3] = R.transpose()
@@ -35,7 +38,8 @@ def getWorld2View(R, t):
     Rt[3, 3] = 1.0
     return np.float32(Rt)
 
-def getWorld2View2(R, t, translate=np.array([.0, .0, .0]), scale=1.0):
+
+def getWorld2View2(R, t, translate=np.array([0.0, 0.0, 0.0]), scale=1.0):
     Rt = np.zeros((4, 4))
     Rt[:3, :3] = R.transpose()
     Rt[:3, 3] = t
@@ -48,30 +52,45 @@ def getWorld2View2(R, t, translate=np.array([.0, .0, .0]), scale=1.0):
     Rt = np.linalg.inv(C2W)
     return np.float32(Rt)
 
-def getProjectionMatrix(znear, zfar, fovX, fovY, cx, cy):
-    tanHalfFovY = math.tan((fovY / 2))
-    tanHalfFovX = math.tan((fovX / 2))
 
-    top = tanHalfFovY * znear
-    bottom = -top
-    right = tanHalfFovX * znear
-    left = -right
+def getProjectionMatrix(fovy, aspect, zNear, zFar):
+    tanHalfFovy = math.tan((fovy / 2.0))
 
-    P = torch.zeros(4, 4)
+    p_matrix = torch.zeros(4, 4)
+    p_matrix[0, 0] = 1.0 / (aspect * tanHalfFovy)
+    p_matrix[1, 1] = 1.0 / tanHalfFovy
+    p_matrix[2, 2] = (zFar + zNear) / (zFar - zNear)
+    p_matrix[2, 3] = 1.0
+    p_matrix[3, 2] = -(2.0 * zFar * zNear) / (zFar - zNear)
+    return p_matrix
 
-    z_sign = 1.0
 
-    P[0, 0] = 2.0 * znear / (right - left)
-    P[1, 1] = 2.0 * znear / (top - bottom)
-    P[0, 2] = cx
-    P[1, 2] = cy
-    P[3, 2] = z_sign
-    P[2, 2] = z_sign * zfar / (zfar - znear)
-    P[2, 3] = -(zfar * znear) / (zfar - znear)
-    return P
+# def getProjectionMatrix(znear, zfar, fovX, fovY, cx, cy):
+#     tanHalfFovY = math.tan((fovY / 2))
+#     tanHalfFovX = math.tan((fovX / 2))
+
+#     top = tanHalfFovY * znear
+#     bottom = -top
+#     right = tanHalfFovX * znear
+#     left = -right
+
+#     P = torch.zeros(4, 4)
+
+#     z_sign = 1.0
+
+#     P[0, 0] = 2.0 * znear / (right - left)
+#     P[1, 1] = 2.0 * znear / (top - bottom)
+#     P[0, 2] = cx
+#     P[1, 2] = cy
+#     P[3, 2] = z_sign
+#     P[2, 2] = z_sign * zfar / (zfar - znear)
+#     P[2, 3] = -(zfar * znear) / (zfar - znear)
+#     return P
+
 
 def fov2focal(fov, pixels):
     return pixels / (2 * math.tan(fov / 2))
 
+
 def focal2fov(focal, pixels):
-    return 2*math.atan(pixels/(2*focal))
+    return 2 * math.atan(pixels / (2 * focal))
